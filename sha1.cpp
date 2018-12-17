25bb8a4a2c1632fd35d3e9dcacc6da7b5d0f4818
LeFrosch <44034965+LeFrosch@users.noreply.github.com>
2018-12-17 21:38:35 +0100

    final

--- sha1.cpp
+++ sha1.cpp
@@ -1,156 +1,138 @@
 #include <iostream>
-#include <bitset>
+#include <iomanip>
 
 using namespace std;
 
-template <size_t size>
-string toHex(bitset<size> in) 
+inline string printh(uint32_t in) 
 {
-    string fix = "";
+    stringstream s;
+    s << hex << in;
+    string result(s.str());
 
-    for (int i = 0; i < size % 4; i++)
-        fix += "0";
-
-    const char *c_fixed = (fix + in.to_string()).c_str();
-
-    int leangth = (fix + in.to_string()).length();
-
-    string result = "";
-    for (int i = 0; i < leangth; i += 4) 
+    for (int i = result.length(); i < 8; i++) 
     {
-        char arr[4];
-        arr[0] = c_fixed[i];
-        arr[1] = c_fixed[i + 1];
-        arr[2] = c_fixed[i + 2];
-        arr[3] = c_fixed[i + 3];
-
-        string test = string(arr);
-
-        if (test == "0000") result += "0";
-        else if (test == "0001") result += "1";
-        else if (test == "0010") result += "2";
-        else if (test == "0011") result += "3";
-        else if (test == "0100") result += "4";
-        else if (test == "0101") result += "5";
-        else if (test == "0110") result += "6";
-        else if (test == "0111") result += "7";
-        else if (test == "1000") result += "8";
-        else if (test == "1001") result += "9";
-        else if (test == "1010") result += "a";
-        else if (test == "1011") result += "b";
-        else if (test == "1100") result += "c";
-        else if (test == "1101") result += "d";
-        else if (test == "1111") result += "e";
+        result = "0" + result;
     }
 
     return result;
 }
 
-template <size_t size>
-inline bitset<size> rotate(bitset<size> &b, unsigned m)
+static uint32_t rol(const uint32_t value, const size_t bits)
 {
-    return b << m | b >> (size - m);
+    return (value << bits) | (value >> (32 - bits));
 }
 
-string sha1(string in) 
+void sha1(string in) 
 {
-    cout << "Input: " << in << endl << endl;
+    cout << endl << "Input: " << in << endl << endl;
 
-    bitset<32> h0("10101001110110101001010101111");
-    bitset<32> h1("11011010100001010010101001001");
-    bitset<32> h2("10101000100010101010101010101");
-    bitset<32> h3("00010100100010101110100101011");
-    bitset<32> h4("10010101010010010111010101110");
+    uint32_t puffers[5];
 
-    cout << "Value h0: " << h0.to_ulong() << endl;
-    cout << "Value h1: " << h1.to_ulong() << endl;
-    cout << "Value h2: " << h2.to_ulong() << endl;
-    cout << "Value h3: " << h3.to_ulong() << endl;
-    cout << "Value h4: " << h4.to_ulong() << endl << endl;
+    puffers[0] = 0x67452301;
+    puffers[1] = 0xefcdab89;
+    puffers[2] = 0x98badcfe;
+    puffers[3] = 0x10325476;
+    puffers[4] = 0xc3d2e1f0;
 
-    bitset<512> h("");
+    cout << "Puffers:" << endl;
+    for (int i = 0; i < 5; i++) 
+    {
+        cout << "   Puffer " << i << ": " << printh(puffers[i]) << endl;
+    }
 
     const char *c_in = in.c_str();
-    for (int i = 0; i < in.length(); i++) 
-    {
-        bitset<8>d(c_in[i]);
+    int size = in.length();
 
-        for (int ii = 0; ii < 8; ii++) 
-        {
-            h[i * 8 + ii] = d[ii];
-        }
+    cout << endl << "Length in bits: " << (size * 8) << endl << endl;
+
+    uint32_t words[80];
+
+    for (int i = 0; i < 80; i++) 
+    {
+        words[i] = 0;
     }
 
-    cout << "Input to number: " << h.to_ullong() << endl << endl;
-
-    bitset<32> words[80];
-
-    for (int i = 0; i < 16; i++) 
+    for (int i = 0; i < size / 4.0; i++) 
     {
-        for (int ii = 0; ii < 32; ii++)
-            words[i][ii] = h[32 * i + ii];
+        words[i] = (c_in[4 * i + 3] & 0xff)
+                 | (c_in[4 * i + 2] & 0xff) << 8
+                 | (c_in[4 * i + 1] & 0xff) << 16
+                 | (c_in[4 * i + 0] & 0xff) << 24;
     }
+    
+    words[size / 4] |= 1 << (31 - (8 * (size % 4)));
+
+    words[15] = size * 8;
 
     for (int i = 16; i < 80; i++) 
     {
         words[i] = words[i - 3] ^ words[i - 8] ^ words[i - 14] ^ words[i - 16];
-        words[i] = rotate<32>(words[i], 1);
+        words[i] = rol(words[i], 1);
     }
 
-    bitset<32> a(h0);
-    bitset<32> b(h0);
-    bitset<32> c(h0);
-    bitset<32> d(h0);
-    bitset<32> e(h0);
-    bitset<32> f;
-    bitset<32> k;
-
-    for (int i = 0; i < 1; i++) 
+    cout << "Initial words: " << endl;
+    for (int i = 0; i < 80; i++) 
     {
+        if (i == 16)
+            cout << endl << "Extended words: " << endl;
+        cout << "   Word " << (i < 10? "0" + to_string(i) : to_string(i)) << ": " << printh(words[i]) << endl;
+    }
+
+    cout << endl << "Main loop:" << endl;
+
+    uint32_t a = puffers[0];
+    uint32_t b = puffers[1];
+    uint32_t c = puffers[2];
+    uint32_t d = puffers[3];
+    uint32_t e = puffers[4];
+
+    for (int i = 0; i < 80; i++) 
+    {
+        uint32_t f;
+        uint32_t k;
+
         if (i < 20) 
         {
             f = (b & c) | ((~b) & d);
-            k = bitset<32>(1518500249);
+            k = 0x5A827999;
         }
         else if (i < 40) 
         {
             f = b ^ c ^ d;
-            k = bitset<32>(1859775393);
+            k = 0x6ED9EBA1;
         }
         else if (i < 60) 
         {
             f = (b & c) | (b & d) | (c & d);
-            k = bitset<32>(2400959708);
+            k = 0x8F1BBCDC;
         }
         else 
         {
             f = b ^ c ^ d;
-            k = bitset<32>(3395469782);
+            k = 0xCA62C1D6;
         }
 
-        bitset<32> temp(rotate<32>(a, 5).to_ullong() + f.to_ullong() + e.to_ullong() + k.to_ullong() + words[i].to_ullong());
+        uint32_t temp = rol(a, 5) + f + e + k + words[i];
         e = d;
         d = c;
-        c = rotate<32>(b, 30);
+        c = rol(b, 30);
         b = a;
         a = temp;
+
+        cout << "[i = " << (i < 10? "0" + to_string(i) : to_string(i)) << "] a: " << printh(a) << " b: " << printh(b) << " c: " << printh(c) << " d: " << printh(d) << " e: " << printh(e) << endl;
     }
 
-    h0 = a;
-    h1 = b;
-    h2 = c;
-    h3 = d; 
-    h4 = e;
+    puffers[0] += a;
+    puffers[1] += b;
+    puffers[2] += c;
+    puffers[3] += d;
+    puffers[4] += e;
 
-    bitset<160> result(h0.to_string() + h1.to_string() + h2.to_string() + h3.to_string() + h4.to_string());
-
-    return toHex(result);
+    cout << endl << "Result: " << printh(puffers[0]) << " " << printh(puffers[1]) << " " << printh(puffers[2]) << " " << printh(puffers[3]) << " " << printh(puffers[4]) << endl;
 }
 
 main(int argc, char const *argv[])
 {
-    string result = sha1("Lola");
-    cout << "Result: " << result << endl;
-
+    sha1(argv[1]);
     return 0;
 }
